73525fdf36cc58da2d0d75be4543bc98f763c851
Allan Aguiar <apodiaguiar11@hotmail.com>
2021-02-04 14:55:12 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,182 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	
+	private Seller entity;
+	
+	private SellerService service;
+	
+	//Como se fosse uma lista de observadores
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		//Adicionando o observador do evento dentro da lista 
+		//Segundo passo do padrão Observer
+		dataChangeListeners.add(listener);
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		
+		//Os dois ifs seria para o caso do programador esquecer de 
+		//injetar a dependência
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");			
+		}
+		
+		if(service == null) {
+			throw new IllegalStateException("Service was null");			
+		}
+		try {
+			//Instanciando o objeto
+			entity = getFormData();
+			
+			//Salvando no Banco de Dados
+			service.saveOrUpdate(entity);
+			
+			//Método para executar o método da interface DataChangeListener
+			//Terceiro passo do padrão Observer
+			notifyDataChangeListeners();
+			
+			//Para fechar a janela, depois de salvar no Banco de dados
+			Utils.currentStage(event).close();
+			
+		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+		
+		
+	}
+	
+	private void notifyDataChangeListeners() {
+		
+		//Quarto passo do padrão Observer
+		//Vai notificar ao observador (a classe SellerListController) que está dentro da lista, 
+		//que houve uma alteração, pois no momento que esse método é chamado,
+		//o conteúdo digitado pelo usuário já foi salvo no Banco de dados
+		for(DataChangeListener listener: dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+	}
+
+	//Método para pegar as informações que estão no TextField e
+	//instanciar um Seller. 
+	private Seller getFormData() {		
+		
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		//Verificando se a String está vazia
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			//Exceção manda uma mensagem dizendo que este campo não pode ser vazio
+			exception.addError("name", "Field can't be empty");
+		}
+		
+		//Mesmo vazio, mas ainda vou setar
+		obj.setName(txtName.getText());
+		
+		//Testando se na coleção de erros tem pelo menos um, caso tenha,
+		//Vai lançar a exceção
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		
+		//Fechar a janela
+		Utils.currentStage(event).close();
+		
+	}
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		initializeNodes();
+		
+	}
+
+	private void initializeNodes() {
+		//Usando regras para a digitação na janelinha de inserção de Departamento
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> field = errors.keySet();
+		
+		if(field.contains("name")) {
+			labelErrorName.setText(errors.get("name"));;
+		}
+		
+	}
+	
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -142,58 +147,58 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
 
-//		// Lógica para abrir a janelinha de formulário
-//		try {
-//
-//			// Carregando a tela
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//
-//			// Pegando o controlador da tela que acabei de carregar
-//			SellerFormController controller = loader.getController();
-//
-//			// Quando pressiono o button new, passo um obj vazio
-//			// por isso, não mostra nada no TextField
-//			// Até por é óbvio, pois estou criando algo
-//			// Injetando um Seller
-//			controller.setSeller(obj);
-//
-//			// Injetando um SellerService
-//			controller.setSellerService(new SellerService());
-//
-//			// Inscrever para escutar o evento do onDataChange
-//			// Aqui inicia o padrão Observer (Primeiro passo)
-//			// Está mandando um objeto do tipo dele mesmo
-//			controller.subscribeDataChangeListener(this);
-//
-//			controller.updateFormData();
-//
-//			// Quando quero mostrar uma janelinha na frente de um Stage (um palco)
-//			// É preciso criar outro palco, pois será um palco em cima de outro
-//			Stage dialogStage = new Stage();
-//
-//			// Título da janelinha
-//			dialogStage.setTitle(" Enter Seller Data ");
-//
-//			// Determinando quem será a cena
-//			dialogStage.setScene(new Scene(pane));
-//
-//			// Fazer com que a janelinha não seja redimensionada
-//			dialogStage.setResizable(false);
-//
-//			// Determinar quem é o Stage pai dessa janelinha
-//			dialogStage.initOwner(parentStage);
-//
-//			// Determina se a janela vai ser modal ou vai ter outro comportamento
-//			// Assim, a janelinha vai ficar travada para que não possa acessar outra coisa
-//			// enquanto essa janelinha estiver aberta
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//
-//			dialogStage.showAndWait();
-//
-//		} catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		// Lógica para abrir a janelinha de formulário
+		try {
+
+			// Carregando a tela
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			// Pegando o controlador da tela que acabei de carregar
+			SellerFormController controller = loader.getController();
+
+			// Quando pressiono o button new, passo um obj vazio
+			// por isso, não mostra nada no TextField
+			// Até por é óbvio, pois estou criando algo
+			// Injetando um Seller
+			controller.setSeller(obj);
+
+			// Injetando um SellerService
+			controller.setSellerService(new SellerService());
+
+			// Inscrever para escutar o evento do onDataChange
+			// Aqui inicia o padrão Observer (Primeiro passo)
+			// Está mandando um objeto do tipo dele mesmo
+			controller.subscribeDataChangeListener(this);
+
+			controller.updateFormData();
+
+			// Quando quero mostrar uma janelinha na frente de um Stage (um palco)
+			// É preciso criar outro palco, pois será um palco em cima de outro
+			Stage dialogStage = new Stage();
+
+			// Título da janelinha
+			dialogStage.setTitle(" Enter Seller Data ");
+
+			// Determinando quem será a cena
+			dialogStage.setScene(new Scene(pane));
+
+			// Fazer com que a janelinha não seja redimensionada
+			dialogStage.setResizable(false);
+
+			// Determinar quem é o Stage pai dessa janelinha
+			dialogStage.initOwner(parentStage);
+
+			// Determina se a janela vai ser modal ou vai ter outro comportamento
+			// Assim, a janelinha vai ficar travada para que não possa acessar outra coisa
+			// enquanto essa janelinha estiver aberta
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
